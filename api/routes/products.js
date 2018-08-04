f512b78bfb2016c6074030e86b48b327e1aba9b9
kanav20 <kanavmalik20@gmail.com>
2018-08-04 22:13:21 +0530

    added few changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
@@ -9,23 +12,39 @@ router.get('/',(req,res,next)=>{
 
 
 router.post('/',(req,res,next)=>{
+    const product = new Product({
+        _id : new mongoose.Types.ObjectId(),
+        name :req.body.name,
+        price : req.body.price
+    });
+    product
+    .save()
+    .then(result =>{
+        console.log(result);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
     res.status(201).json({
         message : "handling POST requests to products",
+        createdProduct : product,
     });
 });
 
 router.get('/:productId',(req,res,next)=>{
     const id = req.params.productId;
-    if(id === 'special') { 
-        res.status(200).json({
-            message : "you discovered the special id ",
-            id : id,
-        });
-    } else {
-        res.status(200).json({
-            message : "there is nothing but shit in here"
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        res.status(200).json(doc);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error : err,
         })
-    }
+    })
 });
 
 
